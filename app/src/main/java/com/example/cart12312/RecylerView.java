928a395d7bd8ea5bb1e9dcec5f40ed25c69f496b
KTC system <xellos_yo@naver.com>
2021-05-16 21:56:12 +0900

    total, recylerview.xml paybutton

--- app/src/main/java/com/example/cart12312/RecylerView.java
+++ app/src/main/java/com/example/cart12312/RecylerView.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,12 +31,18 @@ public class RecylerView extends AppCompatActivity {
     private FirebaseDatabase database; //파이어 베이스 연결
     private DatabaseReference databaseReference;
 
+    Button pay1;
+    TextView pc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recylerview);
 
+
+        pay1 = findViewById(R.id.pay1);
+        pc = findViewById(R.id.pc);
         recyclerView = findViewById(R.id.recyclerView); // 리사이클러뷰 xml 아이디 연결
         recyclerView.setHasFixedSize(true); //리사이클러뷰 기존성능 강화
         layoutManager = new LinearLayoutManager(this);
@@ -45,29 +52,47 @@ public class RecylerView extends AppCompatActivity {
         database = FirebaseDatabase.getInstance(); //파이어베이스 데이터베이스 연동
 
         databaseReference = database.getReference("MainData"); //데이터 베이스 테이블 연결
+
+
+        pay1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+
+
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //파이어베이스 데이터베이스의 데이터를 받아오는 곳
                 arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
-                for (DataSnapshot snapshot:dataSnapshot.getChildren()){  //반복문으로 데이터 List를 추출해냄
+                int total = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {  //반복문으로 데이터 List를 추출해냄
                     MainData maindata = snapshot.getValue(MainData.class);  //만들어뒀던 data 객체에 데이터를 담는다.
                     arrayList.add(maindata); //담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
-
                 }
+
+                //가격 total 값
+                for (int i = 0 ; i < arrayList.size(); i++) {
+                    total +=  arrayList.get(i).getPrice();
+                    pc.setText(String.valueOf(total) + "원");
+                }
+
                 adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                // 디비를 가져오던중 에러 발생 시
                 Log.e("RecyclerView", String.valueOf(databaseError.toException())); // 에러문 출력
-
             }
         });
 
-             adapter = new MainAdapter(arrayList, this);
-             recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
+        adapter = new MainAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter); //리사이클러뷰에 어댑터 연결
     }
 
 }
